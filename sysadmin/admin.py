2497208895f31f1169929e6b5a500dd318ff2e82
TheLunaticDev <luna@thelunaticdev.com>
2024-10-16 10:43:42 +0530

    Changed UI to filter_horizontal for subordinates field of Manager model

--- sysadmin/admin.py
+++ sysadmin/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Manager
 
+class ManagerAdmin(admin.ModelAdmin):
+    model = Manager
+    filter_horizontal = ['subordinates']
+
 admin.site.register(Manager)
