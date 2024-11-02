d803a209dff90f9c733b61b1fb9872e061c5eeb7
TheLunaticDev <sushantabanerjee@zohomail.in>
2024-11-02 16:20:09 +0530

    Added Infinite Scrolling to CDB view

--- cdb/urls.py
+++ cdb/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view, name='cdb_index_view'),
-    path('add_to_tp/', views.select_view, name='cdb_select_view'),
+    path('cdbentry/', views.cdbentry_list, name='cdbentry_list'),
+    path('cdbentry/select/', views.cdbentry_list_for_select_view, name='cdbentry_list_for_select_view'),
+    path('select/', views.select_view, name='cdb_select_view'),
     path('popover-content/<int:entry_id>', views.cdb_popover_content, name='cdb_popover_content'),
 ]
--- cdb/views.py
+++ cdb/views.py
@@ -1,6 +1,8 @@
+from urllib.parse import urlparse
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse
+from django.urls import reverse, resolve
 from django.db.models import Q
 from .models import CDBEntry
 
@@ -15,55 +17,67 @@ def cdb_popover_content(request, entry_id):
     }
     return render(request, 'crm/includes/_popover_content.html', context)
 
-def get_cdb_entry_context(request):
-    query = request.GET.get('q')
-    area = request.GET.get('area')
-    district = request.GET.get('district')
-    state = request.GET.get('state')
-
-    entries = CDBEntry.objects.all()
+def get_cdb_entries(request):
+    cdbentries = CDBEntry.objects.all().order_by('owner')
     
-    if query:
-        entries = entries.filter(
-            Q(institute__icontains=query) |
-            Q(area__name__icontains=query) |
-            Q(doctors__name__icontains=query) |
-            Q(doctors__speciality__icontains=query) |
-            Q(doctors__designation__icontains=query) |
-            Q(doctors__phone_number__icontains=query) |
-            Q(admins__name__icontains=query) |
-            Q(admins__designation__icontains=query) |
-            Q(admins__email__icontains=query) |
-            Q(admins__phone_number__icontains=query) |
-            Q(references__name__icontains=query) |
-            Q(references__email__icontains=query) |
-            Q(references__phone_number__icontains=query)
-        )
-
-    if area:
-        entries = entries.filter(area=area)
-
-    if district:
-        entries = entries.filter(district=district)
-
-    if state:
-        entries = entries.filter(state=state)
-
-    return {
-        'entries': entries,
-    }
+    return cdbentries
 
 @login_required
 def index_view(request):
-    context = get_cdb_entry_context(request)
+    entries_per_page = 50 
+    page_number = request.GET.get('page', 1)
+    
+    entries = get_cdb_entries(request)
+    paginator = Paginator(entries, entries_per_page)
+
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,
+    }
     return render(request, 'cdb/index_view.html', context)
 
 @login_required
-def edit_view(request):
-    context = get_cdb_entry_context(request)
-    return render(request, 'cdb/edit_view.html', context)
+def cdbentry_list(request):
+    entries_per_page = 50
+    page_number = request.GET.get('page', 1)
+    
+    entries = get_cdb_entries(request)
+    paginator = Paginator(entries, entries_per_page)
+
+    page_obj = paginator.get_page(page_number)
+
+    print(request.GET)
+
+    return render(request, "cdb/partials/cdbentry_list.html", {"page_obj": page_obj})
+
+@login_required
+def cdbentry_list_for_select_view(request):
+
+    print("CDBEntry for Select View Triggered........................")
+    entries_per_page = 50
+    page_number = request.GET.get('page', 1)
+    
+    entries = get_cdb_entries(request)
+    paginator = Paginator(entries, entries_per_page)
+    
+    page_obj = paginator.get_page(page_number)
+
+    for entry in page_obj:
+        print(entry)
+    
+    return render(request, "cdb/partials/select/cdbentry_list.html", {"page_obj": page_obj})
 
 @login_required
 def select_view(request):
-    context = get_cdb_entry_context(request)
-    return render(request, 'cdb/cdb_select_view.html', context)
+    entries_per_page = 50
+    page_number = request.GET.get('page', 1)
+
+    entries = get_cdb_entries(request)
+    paginator = Paginator(entries, entries_per_page)
+    
+    page_obj = paginator.get_page(page_number)
+    
+    print("Entered Select View........................")
+
+    return render(request, 'cdb/select_view.html', {"page_obj": page_obj})
