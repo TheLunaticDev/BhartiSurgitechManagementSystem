1c40717ef493d933ba7d1909df6ec09c295d95bd
TheLunaticDev <sushantabanerjee@zohomail.in>
2024-11-08 20:29:57 +0530

    Urgent fix so that managers can edit crm entries

--- crm/admin.py
+++ crm/admin.py
@@ -114,7 +114,7 @@ class EntryAdmin(admin.ModelAdmin):
     list_display = ['id', 'institute', 'time_since_creation', 'owner']
 
     fieldsets = [
-        ('Entry Information', {'fields': [('institute', 'area', 'stage'), ('expected', 'notes', 'schedule_date'), ('landmark', 'hospital_type', 'sector', 'discipline')], 'classes': ['collapse']}),
+        ('Entry Information', {'fields': [('institute', 'area', 'stage'), ('expected', 'notes'), ('landmark', 'hospital_type', 'sector', 'discipline')], 'classes': ['collapse']}),
     ]
     readonly_fields = ['created_on', 'owner']
     inlines = [ProductEntryInline, AdministratorInline, DoctorInline, ReferenceInline]
