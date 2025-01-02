240dc3216161055a9c57947c3804665456d28470
TheLunaticDev <luna@thelunaticdev.com>
2025-01-02 20:00:22 +0530

    Implementing CI/CD.

--- BhartiManagementSystem/asgi.py
+++ BhartiManagementSystem/asgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+from dotenv import load_dotenv
+
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BhartiManagementSystem.settings')
+
+load_dotenv()
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    os.getenv('DJANGO_SETTINGS_MODUE', 'BhartiManagementSystem.settings.base'))
 
 application = get_asgi_application()
